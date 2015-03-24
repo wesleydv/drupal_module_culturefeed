38dd87a69062b85c9cbe18ff441f5812ea0ebd1f
Jochen Stals <stalski@322618.no-reply.drupal.org>
2015-03-24 13:54:16 +0100

    added first part of the calendar facets.

---
+++ culturefeed_calendar/lib/Drupal/CultureFeedMyCalendarPage.php
@@ -0,0 +1,315 @@
+<?php
+/**
+ * @file
+ * Defines a Page callback for Calendar search results for a user.
+ */
+
+use \CultuurNet\Search\Parameter;
+use \CultuurNet\Search\Component\Facet;
+
+/**
+ * Class CultureFeedMyCalendarPage
+ */
+class CultureFeedMyCalendarPage extends CultureFeedSearchPage
+    implements CultureFeedSearchPageInterface {
+
+  /**
+   * Initializes the search with data from the URL query parameters.
+   */
+  public function initialize() {
+
+    // Only initialize once.
+    if (empty($this->facetComponent)) {
+      $this->facetComponent = new Facet\FacetComponent();
+
+      // Retrieve search parameters and add some defaults.
+      $params = drupal_get_query_parameters();
+      $params += array(
+        'sort' => $this->getDefaultSort(),
+        'search' => '',
+        'facet' => array(),
+      );
+
+      $this->pageNumber = empty($params['page']) ? 1 : $params['page'] + 1;
+
+      if (!empty($params['search'])) {
+        // Remove / from the start and : from the end of keywords.
+        $this->addQueryTerm(preg_replace("/\/\b|\b:/x", "", $params['search']));
+      }
+
+      $add_type_filter = TRUE;
+      if (isset($params['facet']['type'])) {
+        $active_types = $params['facet']['type'];
+        unset($params['facet']['type']);
+      }
+      else {
+        $active_types = variable_get('culturefeed_agenda_active_entity_types', array('event', 'production'));
+        // If all active types are selected, don't add filter.
+        if (count($active_types) == count(culturefeed_agenda_known_entity_types())) {
+          $add_type_filter = FALSE;
+        }
+      }
+
+      if ($add_type_filter) {
+        array_walk($active_types, function(&$active_type) {
+          $active_type = 'type:' . $active_type;
+        });
+        $this->parameters[] = new Parameter\FilterQuery(implode(' OR ', $active_types));
+      }
+
+      $this->addFacetFilters($params);
+
+      $this->parameters[] = $this->facetComponent->facetField('category');
+      $this->parameters[] = $this->facetComponent->facetField('datetype');
+      $this->parameters[] = $this->facetComponent->facetField('city');
+      $this->parameters[] = $this->facetComponent->facetField('location_category_facility_id');
+
+      $this->execute($params);
+
+      // Warm up cache.
+      $this->warmupCache();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function addFacetFilters($params) {
+    parent::addFacetFilters($params);
+  }
+
+  /**
+   * Get the active trail to show.
+   */
+  public function getActiveTrail() {
+
+    $active_trail = array();
+
+    $active_trail[] = array(
+      'title' => t('Home'),
+      'href' => '<front>',
+      'link_path' => '',
+      'localized_options' => array(),
+      'type' => 0,
+    );
+
+    $active_trail[] = array(
+      'title' => t('Your Account'),
+      'href' => 'user',
+      'link_path' => '',
+      'localized_options' => array(),
+      'type' => 0,
+    );
+
+    // Show event type and theme in breadcrumb.
+    $query = drupal_get_query_parameters(NULL, array('page', 'q'));
+    $facet = array();
+    if (isset($query['facet']['category_eventtype_id']) || isset($query['facet']['category_theme_id'])) {
+
+      if (isset($query['facet']['category_eventtype_id'])) {
+
+        $facet['category_eventtype_id'] = $query['facet']['category_eventtype_id'];
+
+        $active_trail[] = array(
+          'title' => culturefeed_search_get_term_translation($query['facet']['category_eventtype_id'][0]),
+          'href' => 'agenda/search',
+          'link_path' => '',
+          'localized_options' => array(
+            'query' => array(
+              'facet' => $facet,
+            ),
+          ),
+          'type' => 0,
+        );
+      }
+
+      if (isset($query['facet']['category_theme_id'])) {
+
+        $facet['category_theme_id'] = $query['facet']['category_theme_id'];
+
+        $active_trail[] = array(
+          'title' => culturefeed_search_get_term_translation($query['facet']['category_theme_id'][0]),
+          'href' => 'agenda/search',
+          'link_path' => '',
+          'localized_options' => array(
+            'query' => array(
+              'facet' => $facet,
+            ),
+          ),
+          'type' => 0,
+        );
+      }
+
+    }
+
+    if (isset($query['location'])) {
+
+      $active_trail[] = array(
+        'title' => $query['location'],
+        'href' => 'agenda/search',
+        'link_path' => '',
+        'localized_options' => array(
+          'query' => array(
+            'location' => $query['location'],
+            'facet' => $facet,
+          ),
+        ),
+        'type' => 0,
+      );
+
+    }
+
+    $active_trail[] = array(
+      'title' => $this->getPageTitle(),
+      'href' => $_GET['q'],
+      'link_path' => '',
+      'localized_options' => array(),
+      'type' => 0,
+    );
+
+    return $active_trail;
+
+  }
+
+  /**
+   * Warm up static caches that will be needed for this request.
+   * We do this before rendering, because data for by example the recommend link would generate 20 requests.
+   * This way we can lower this to only 1 request.
+   */
+  protected function warmupCache() {
+    $this->translateFacets();
+    $this->prepareSlugs();
+  }
+
+  /**
+   * Warm up cache for facets to translate the items.
+   */
+  private function translateFacets() {
+    $found_ids = array();
+    $found_results = array();
+    $translated_terms = array();
+    $facets = $this->facetComponent->getFacets();
+    foreach ($facets as $key => $facet) {
+      // The key should start with 'category_' or 'location_'
+      $start = substr($key, 0, 9);
+      if (in_array($start, array('category_', 'location_'))) {
+        $items = $facet->getResult()->getItems();
+        foreach ($items as $item) {
+          $found_ids[$item->getValue()] = $item->getValue();
+        }
+      }
+    }
+
+    // Translate the facets.
+    if ($translations = culturefeed_search_term_translations($found_ids, TRUE)) {
+
+      // Preferred language.
+      $preferred_language = culturefeed_search_get_preferred_language();
+
+      // Translate the facets labels.
+      foreach ($facets as $key => $facet) {
+        // The key should start with 'category_' or 'location_'
+        $start = substr($key, 0, 9);
+        if (in_array($start, array('category_', 'location_'))) {
+          $items = $facet->getResult()->getItems();
+          foreach ($items as $item) {
+            // Translate if found.
+            if (!empty($translations[$item->getValue()][$preferred_language])) {
+              $item->setLabel($translations[$item->getValue()][$preferred_language]);
+            }
+          }
+        }
+      }
+
+    }
+
+  }
+
+  /**
+   * Prepare slugs for culturefeed_agenda_url_outbound_alter().
+   */
+  private function prepareSlugs() {
+    $term_slugs = &drupal_static('culturefeed_search_term_slugs', array());
+    $facets = $this->facetComponent->getFacets();
+    $items = array();
+
+    // At the moment we only need slugs for event type and themes.
+    if (isset($facets['category_eventtype_id'])) {
+      $items = $facets['category_eventtype_id']->getResult()->getItems();
+    }
+    if (isset($facets['category_theme_id'])) {
+      $items = array_merge($items, $facets['category_theme_id']->getResult()->getItems());
+    }
+
+    // Search the slug for all facet items.
+    if ($items) {
+
+      $preferred_language = culturefeed_search_get_preferred_language();
+
+      // Construct an array with tids to do the query.
+      $tids = array();
+      foreach ($items as $item) {
+        $subitems = $item->getSubItems();
+        if ($subitems) {
+          foreach ($subitems as $subitem) {
+            $tids[] = $subitem->getValue();
+          }
+        }
+        $tids[] = $item->getValue();
+      }
+
+      $result = db_query('SELECT tid, slug FROM {culturefeed_search_terms} WHERE tid IN(:tids) AND language = :language', array(':tids' => $tids, ':language' => $preferred_language));
+      foreach ($result as $row) {
+        $term_slugs[$row->tid] = $row->slug;
+      }
+    }
+
+  }
+
+  /**
+   * Gets a page description for all pages.
+   *
+   * Only type aanbod UiT domein, theme and location need to be prepared for search engines.
+   *
+   * @see culturefeed_search_ui_search_page()
+   *
+   * @return string
+   *   Description for this type of page.
+   */
+  public function getPageDescription() {
+
+    $message = "";
+
+    $query = drupal_get_query_parameters(NULL, array('q'));
+
+    if (empty($query)) {
+      $message = t("A summary of all events and productions");
+    }
+    else {
+      $message = t("A summary of all events and productions");
+
+      if (!empty($query['regId'])) {
+        $term = culturefeed_search_get_term_translation($query['regId']);
+        $message .= t(" in @region", array('@region' => $term));
+      }
+      elseif (!empty($query['location'])) {
+        $message .= t(" in @region", array('@region' => $query['location']));
+      }
+
+      if (!empty($query['facet']['category_eventtype_id'][0])) {
+        $term = culturefeed_search_get_term_translation($query['facet']['category_eventtype_id'][0]);
+        $message .= t(" of the type @type", array('@type' => $term));
+      }
+
+      if (!empty($query['facet']['category_theme_id'][0])) {
+        $term = culturefeed_search_get_term_translation($query['facet']['category_theme_id'][0]);
+        $message .= t(" with theme @theme", array('@theme' => $term));
+      }
+
+    }
+
+    return $message;
+  }
+
+}
+
