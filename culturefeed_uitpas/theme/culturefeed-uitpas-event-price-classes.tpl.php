deb341a7703cdad63517640b3023d4c9560025b1
HnLn <hans@2dotstwice.be>
2016-01-05 15:50:03 +0100

    MSS-231 Use uitpas distribution keys for price details.

---
+++ culturefeed_uitpas/theme/culturefeed-uitpas-event-price-classes.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @file
+* Default theme implementation to display the price classes on an event detail.
+*
+* Available variables:
+* - $price_classes: the price classes.
+* - $price_classes_default_render: the default render of the price classes.
+**/
+?>
+<div class="event-price-classes">
+  <?php print $price_classes_default_render; ?>
+</div>
